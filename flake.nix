46ccc95d7ca3bdda47ee9cf92fc2538512f163e8
ArtemiiAkhunov <artemii13052003@gmail.com>
2026-07-28 22:58:58 -0700

    Updated underlying nix flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   description = "Laggy World website";
 
-  outputs = { nixpkgs, ... }:
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      systemArch = "x86_64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${systemArch};
     in {
-      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      packages.${systemArch}.default = pkgs.stdenv.mkDerivation {
         name = "website";
         src = ./.;
         nativeBuildInputs = [ pkgs.zola ];
